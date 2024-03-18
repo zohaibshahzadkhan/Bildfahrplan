deaa0e966d811c5bfdde1d34f8d1a8f6a35b5a7f
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-03-18 20:59:07 +0100

    :hammer: Add Constant file

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+// constants.js
+
+// Route ID mappings for display purposes
+
+export const ROUTE_ID_NAME_MAPPING = {
+  '1': 'S3',
+  '2': 'S4',
+  '3': 'S5',
+  '4': 'S6',
+  '5': 'S1',
+  '6': 'S2',
+  '7': 'S60',
+  '8': 'S11'
+};
+
+// Starting time for filtering stop times
+export const START_TIME = '06:00:00';
+
+// Ending time for filtering stop times
+export const END_TIME = '12:00:00';
+
